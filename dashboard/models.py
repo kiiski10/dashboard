c84790d55eb321e7c6070627d09c44b240a70979
kiiski10 <mavakatti@gmail.com>
2021-08-24 20:33:18 +0300

    move secret key to untracked file

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=20)
+    state = models.CharField(max_length=15)
--- paku/settings.py
+++ paku/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from . import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n8@7ar4nfmrwss5gn_qw5zn7jt+hh&b%rc^+1raro+y0_t))zj'
+SECRET_KEY = secrets.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
