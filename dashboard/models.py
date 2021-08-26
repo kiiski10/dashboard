3dabddb4e8b1a0bc7ac3fa44113c12f6c13f317a
kiiski10 <mavakatti@gmail.com>
2021-08-26 10:50:56 +0300

    remove some .pyc files from git

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Car(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=20)
-    state = models.CharField(max_length=15)
+    image = models.ImageField()
