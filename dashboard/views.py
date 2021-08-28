75b0a348363fb21a8b45af62d980288b754815d2
kiiski10 <mavakatti@gmail.com>
2021-08-28 09:47:37 +0300

    add template for dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import Car
 
 def index(request):
-    return HttpResponse("""Hello, human. Im your car""")
+    cars = Car.objects.order_by('-name')[:5]
+    template = loader.get_template('dashboard/index.html')
+    context = {
+        'cars': cars,
+    }
+    return HttpResponse(template.render(context, request))
