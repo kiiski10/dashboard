2e3f68e0440d0d5cd31ed29936502d922d6578cd
kiiski10 <mavakatti@gmail.com>
2021-08-28 11:12:48 +0300

    make Cars editable in admin view

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 class Car(models.Model):
     name = models.CharField(max_length=200)
-    location = models.CharField(max_length=20)
-    image = models.ImageField()
+    location = models.CharField(max_length=20, blank=True)
+    image = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Car"
+
