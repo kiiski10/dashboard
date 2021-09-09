cffb678e3fef0aaa23acd72bfbf3a915fd20c41f
kiiski10 <mavakatti@gmail.com>
2021-09-09 20:51:04 +0300

    add view and template for single car

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,3 +5,8 @@ def index(request):
     cars = Car.objects.all()
     context = { "cars": cars }
     return render(request, "dashboard/index.html", context)
+
+def car(request, id):
+    car = Car.objects.get(pk=id)
+    context = { "car": car }
+    return render(request, "dashboard/car.html", context)
