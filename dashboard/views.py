f35c4f0e05a4a40d012fc0db9b95ae3f11329118
kiiski10 <mavakatti@gmail.com>
2021-09-09 19:31:49 +0300

    fix index.html template to show Car models

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,7 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Car
 
 def index(request):
-    cars = Car.objects.order_by('-name')[:5]
-    template = loader.get_template('dashboard/index.html')
-    context = {
-        'cars': cars,
-    }
-    return HttpResponse(template.render(context, request))
+    cars = Car.objects.all()
+    context = { "cars": cars }
+    return render(request, "dashboard/index.html", context)
